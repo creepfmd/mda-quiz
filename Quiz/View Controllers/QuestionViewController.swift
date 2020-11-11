bd50b7d1d7e732aa2b8b996a66e2571e93f32d84
Sergey Mityanin <s.mityanin@gmail.com>
2020-11-11 23:25:32 +0300

    Single answers done

--- Quiz/View Controllers/QuestionViewController.swift
+++ Quiz/View Controllers/QuestionViewController.swift
@@ -8,23 +8,24 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-    @IBOutlet weak var rangeStackView: UIStackView!
-    @IBOutlet weak var multipleStackView: UIStackView!
-    @IBOutlet weak var singleStackView: UIStackView!
-    
     @IBOutlet weak var questionTextLabel: UILabel!
     
-    @IBOutlet weak var singleFirstAnswerButton: UIButton!
-    @IBOutlet weak var singleSecondAnswerButton: UIButton!
-    @IBOutlet weak var singleThirdAnswerButton: UIButton!
-    @IBOutlet weak var singleFourthAnswerButton: UIButton!
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+        
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet weak var rangeStackView: UIStackView!
+    @IBOutlet weak var rangeSlider: UISlider!
+    @IBOutlet weak var rangeLeftLabel: UILabel!
+    @IBOutlet weak var rangeRightLabel: UILabel!
     
     @IBOutlet weak var progressBar: UIProgressView!
 
     var questionIndex = 0
-    var progress: Float {
-        return Float(questionIndex) / Float(Question.all.count)
-    }
+    var userAnswers = [Answer]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,31 +39,79 @@ class QuestionViewController: UIViewController {
         }
         
         navigationItem.title = "Вопрос №\(questionIndex + 1)"
-        progressBar.progress = progress
         
         let question = Question.all[questionIndex]
+        let totalProgress = Float(questionIndex) / Float(Question.all.count)
         
         questionTextLabel.text = question.text
+        progressBar.setProgress(totalProgress, animated: true)
         
         switch question.type {
         case .single:
-            singleFirstAnswerButton.setTitle(question.answers[0].text, for: [])
-            singleSecondAnswerButton.setTitle(question.answers[1].text, for: [])
-            singleThirdAnswerButton.setTitle(question.answers[2].text, for: [])
-            singleFirstAnswerButton.setTitle(question.answers[3].text, for: [])
-            singleStackView.isHidden = false
+            updateSingleStack()
         case .multiple:
-            multipleStackView.isHidden = false
+            updateMultipleStack()
         case .range:
-            rangeStackView.isHidden = false
+            updateRangeStack()
         }
-        
-        questionIndex = (questionIndex + 1) % Question.all.count
     }
     
-    @IBAction func singleAnswerClicked(_ sender: UIButton) {
+    func updateSingleStack() {
+        let answers = getCurrentQuestionAnswers()
+        for (index, button) in singleButtons.enumerated() {
+            button.setTitle(nil, for: [])
+            button.tag = index
+        }
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.text, for: [])
+        }
+        singleStackView.isHidden = false
+    }
+    
+    func updateMultipleStack() {
+        let answers = getCurrentQuestionAnswers()
+        for (index, label) in multipleLabels.enumerated() {
+            label.text = ""
+            // TODO: set tags for switches
+        }
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.text
+        }
+        multipleStackView.isHidden = false
+    }
+    
+    func updateRangeStack() {
+        let answers = getCurrentQuestionAnswers()
+        rangeLeftLabel.text = answers.first?.text
+        rangeRightLabel.text = answers.last?.text
+        rangeStackView.isHidden = false
+    }
+    
+    func nextQuestion() {
+        print(userAnswers)
+        questionIndex = (questionIndex + 1) % Question.all.count
         updateUI()
     }
     
-
+    func getCurrentQuestionAnswers() -> [Answer] {
+        return Question.all[questionIndex].answers
+    }
+    
+    @IBAction func singleAnswerClicked(_ sender: UIButton) {
+        let answers = getCurrentQuestionAnswers()
+        let index = sender.tag
+        guard index >= 0 && index < answers.count else {
+            return
+        }
+        userAnswers.append(answers[index])
+        nextQuestion()
+    }
+    
+    @IBAction func multipleAnswerClicked(_ sender: Any) {
+        nextQuestion()
+    }
+    
+    @IBAction func rangeAnswerClicked(_ sender: Any) {
+        nextQuestion()
+    }
 }
