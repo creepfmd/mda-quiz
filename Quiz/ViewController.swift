fb0c3b74c855dab496bf106f216cd1712b755d04
Sergey Mityanin <s.mityanin@gmail.com>
2020-11-11 13:50:31 +0300

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Сергей on 11.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
